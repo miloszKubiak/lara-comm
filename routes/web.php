aee5380b41352473dd911afd1b9f98a91186385c
miloszKubiak <milypan69@gmail.com>
2021-12-06 19:52:08 +0100

    add user registration

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/posts', function () {
     return view('posts.index');
